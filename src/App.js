51bc307d057ad5ca0352e5c6aaa2c0be1112bfff
redavillm <leonovvoleg@yandex.ru>
2023-07-24 00:24:18 +0300

    разбивю на компоненты

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
+import { useState } from "react";
 import "./App.css";
 
 const numArr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-let move = true;
+let move = false;
+
+const Cell = (props) => {
+  let [sym, setSym] = useState("");
+
+  return (
+    <div
+      className="field"
+      id={props.number}
+      onClick={() => {
+        move === true ? setSym((sym = "x")) : setSym((sym = "o"));
+      }}
+    >
+      {sym}
+    </div>
+  );
+};
 
 function App() {
   return (
     <div className="App">
       <div className="move">Сейчас ход: {move ? "X" : "O"}</div>
       <div className="grid">
-        {numArr.map(el => (
-          <div className="field" id={el}>O</div>
+        {numArr.map((el) => (
+          <Cell number={el} />
         ))}
       </div>
       <button className="reset">Reset</button>
