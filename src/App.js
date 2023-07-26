1177c4d8cce05705c1f1fafe35e11093dad47b7b
redavillm <leonofforc@yandex.ru>
2023-07-26 21:52:46 +0300

    разные подходы как reset

--- src/App.js
+++ src/App.js
@@ -3,32 +3,52 @@ import "./App.css";
 
 const numArr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-let move = false;
-
-const Cell = (props) => {
-  let [sym, setSym] = useState("");
+const Cell = ({ num, index, setIndex, value }) => {
+  let [localValue, setLocalValue] = useState();
 
   return (
     <div
-      className="field"
-      id={props.number}
+      className="cell"
+      id={num}
       onClick={() => {
-        move === true ? setSym((sym = "x")) : setSym((sym = "o"));
+        if (!localValue) {
+          if (index) {
+            setIndex(!index);
+            setLocalValue((localValue = "x"));
+          } else {
+            setIndex(!index);
+            setLocalValue((localValue = "o"));
+          }
+        }
       }}
     >
-      {sym}
+      {localValue}
     </div>
   );
 };
 
+const setValue = (index) => {
+  return index ? "x" : "o";
+};
+
 function App() {
+  // let [value, setValue] = useState('');
+  let [index, setIndex] = useState(true);
+  let [cell1, setCell1] = useState("");
+
   return (
     <div className="App">
-      <div className="move">Сейчас ход: {move ? "X" : "O"}</div>
+      <div className="index">Сейчас ход: {index ? "X" : "O"}</div>
       <div className="grid">
-        {numArr.map((el) => (
-          <Cell number={el} />
-        ))}
+        {/* {numArr.map((el) => (
+          <Cell num={el} index={index} setIndex={setIndex} />
+        ))} */}
+        <div
+          className="cell"
+          onClick={setCell1(!cell1 ? setCell1(setValue(index)) : cell1)}
+        >
+          {cell1}
+        </div>
       </div>
       <button className="reset">Reset</button>
     </div>
